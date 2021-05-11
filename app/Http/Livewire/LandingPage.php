e1d8d2501aca07dda6682d432c3e789bb1ca49ad
saif1642 <islumsaiful@gmail.com>
2021-05-11 11:16:34 +0600

    form submit action

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -3,13 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Subscriber;
 
 class LandingPage extends Component
 {
-    public $email;
+    public $email = 'test@example.com';
 
     public function subscribe(){
-        \Log::debug($this->email);
+        //\Log::debug($this->email);
+        $subscriber = Subscriber::create([
+            'email' => $this->email
+        ]);
+        $this->reset('email');
     }
 
     public function render()
